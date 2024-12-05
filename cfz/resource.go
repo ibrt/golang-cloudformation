dcbdf5a0a7147f3669d819a61b8c0ee30a34186b
Ivan Bertona <ivan.bertona@gmail.com>
2024-12-05 21:47:02 +0100

    Add some resource policies.

--- cfz/resource.go
+++ cfz/resource.go
@@ -23,3 +23,67 @@ type Resource interface {
 	GetType() string
 	json.Marshaler
 }
+
+// ResourceDeletionPolicy describes a CloudFormation resource deletion policy.
+type ResourceDeletionPolicy interface {
+	GetResourceDeletionPolicy() string
+	json.Marshaler
+}
+
+type resourceDeletionPolicy string
+
+// GetResourceDeletionPolicy implements the ResourceDeletionPolicy interface.
+func (p resourceDeletionPolicy) GetResourceDeletionPolicy() string {
+	return string(p)
+}
+
+// MarshalJSON implements the ResourceDeletionPolicy interface.
+func (p resourceDeletionPolicy) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.GetResourceDeletionPolicy())
+}
+
+var (
+	// ResourceDeletionPolicies provides quick access to the supported CloudFormation resource deletion policies.
+	ResourceDeletionPolicies = struct {
+		Delete               ResourceDeletionPolicy
+		Snapshot             ResourceDeletionPolicy
+		RetainExceptOnCreate ResourceDeletionPolicy
+		Retain               ResourceDeletionPolicy
+	}{
+		Delete:               resourceDeletionPolicy("Delete"),
+		Snapshot:             resourceDeletionPolicy("Snapshot"),
+		RetainExceptOnCreate: resourceDeletionPolicy("RetainExceptOnCreate"),
+		Retain:               resourceDeletionPolicy("Retain"),
+	}
+)
+
+// ResourceUpdateReplacePolicy describes a CloudFormation resource update replace policy.
+type ResourceUpdateReplacePolicy interface {
+	GetResourceUpdateReplacePolicy() string
+	json.Marshaler
+}
+
+type resourceUpdateReplacePolicy string
+
+// GetResourceUpdateReplacePolicy implements the ResourceUpdateReplacePolicy interface.
+func (p resourceUpdateReplacePolicy) GetResourceUpdateReplacePolicy() string {
+	return string(p)
+}
+
+// MarshalJSON implements the ResourceUpdateReplacePolicy interface.
+func (p resourceUpdateReplacePolicy) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.GetResourceUpdateReplacePolicy())
+}
+
+var (
+	// ResourceUpdateReplacePolicies provides quick access to the supported update replace policies.
+	ResourceUpdateReplacePolicies = struct {
+		Delete   ResourceUpdateReplacePolicy
+		Snapshot ResourceUpdateReplacePolicy
+		Retain   ResourceUpdateReplacePolicy
+	}{
+		Delete:   resourceUpdateReplacePolicy("Delete"),
+		Snapshot: resourceUpdateReplacePolicy("Snapshot"),
+		Retain:   resourceUpdateReplacePolicy("Retain"),
+	}
+)
