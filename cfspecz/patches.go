17da98dcb698478bde12f1c7b3d6c7218afa7cff
Ivan Bertona <ivan.bertona@gmail.com>
2024-12-08 16:26:29 +0100

    Small tweaks.

--- cfspecz/patches.go
+++ cfspecz/patches.go
@@ -7,7 +7,8 @@ import (
 // NewDefaultPatchManager initializes a new patch manager and configures it with default, known patchers.
 func NewDefaultPatchManager() *PatchManager {
 	return NewPatchManager().
-		// Special treatment for "CustomResource", clients can handle it by implementing the cfz.Resource interface.
+		// Special treatment for "CustomResource".
+		// Clients can handle it by implementing the cfz.Resource interface.
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::CloudFormation::CustomResource",
 		}).
@@ -18,7 +19,8 @@ func NewDefaultPatchManager() *PatchManager {
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::CloudWatch::InsightRule.Tags",
 		}).
-		// References invalid, deleted type "AWS::CloudWatch::InsightRule.Tags", fallback to List(Tag) (as documented)..
+		// References invalid, deleted type "AWS::CloudWatch::InsightRule.Tags",
+		// fallback to List(Tag) (as documented)..
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::CloudWatch::InsightRule",
 			PropertyName: "Tags",
@@ -39,7 +41,8 @@ func NewDefaultPatchManager() *PatchManager {
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::CodeBuild::Project.FilterGroup",
 		}).
-		// References invalid, deleted type "AWS::CodeBuild::Project.FilterGroup", fallback to JSON.
+		// References invalid, deleted type "AWS::CodeBuild::Project.FilterGroup",
+		// fallback to JSON.
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::CodeBuild::Project.ProjectTriggers",
 			PropertyName: "FilterGroups",
@@ -60,7 +63,8 @@ func NewDefaultPatchManager() *PatchManager {
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::DLM::LifecyclePolicy.CrossRegionCopyTargets",
 		}).
-		// References invalid, deleted type "AWS::DLM::LifecyclePolicy.CrossRegionCopyTargets", fallback to JSON.
+		// References invalid, deleted type "AWS::DLM::LifecyclePolicy.CrossRegionCopyTargets",
+		// fallback to JSON.
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::DLM::LifecyclePolicy",
 			PropertyName: "CrossRegionCopyTargets",
@@ -77,7 +81,8 @@ func NewDefaultPatchManager() *PatchManager {
 				ItemType:          "",
 			},
 		}).
-		// References invalid, deleted type "AWS::DLM::LifecyclePolicy.CrossRegionCopyTargets", fallback to JSON.
+		// References invalid, deleted type "AWS::DLM::LifecyclePolicy.CrossRegionCopyTargets",
+		// fallback to JSON.
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::DLM::LifecyclePolicy.PolicyDetails",
 			PropertyName: "CrossRegionCopyTargets",
@@ -98,7 +103,8 @@ func NewDefaultPatchManager() *PatchManager {
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::DLM::LifecyclePolicy.ExcludeTags",
 		}).
-		// References invalid, deleted type "AWS::DLM::LifecyclePolicy.ExcludeTags", fallback to JSON.
+		// References invalid, deleted type "AWS::DLM::LifecyclePolicy.ExcludeTags",
+		// fallback to JSON.
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::DLM::LifecyclePolicy.Exclusions",
 			PropertyName: "ExcludeTags",
@@ -119,7 +125,8 @@ func NewDefaultPatchManager() *PatchManager {
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::DLM::LifecyclePolicy.ExcludeVolumeTypesList",
 		}).
-		// References invalid, deleted type "AWS::DLM::LifecyclePolicy.ExcludeVolumeTypesList", fallback to JSON.
+		// References invalid, deleted type "AWS::DLM::LifecyclePolicy.ExcludeVolumeTypesList",
+		// fallback to JSON.
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::DLM::LifecyclePolicy.Exclusions",
 			PropertyName: "ExcludeVolumeTypes",
@@ -143,7 +150,8 @@ func NewDefaultPatchManager() *PatchManager {
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::Glue::Table.MetadataOperation",
 		}).
-		// References invalid, deleted type "AWS::Glue::Table.MetadataOperation", fallback to String (as documented).
+		// References invalid, deleted type "AWS::Glue::Table.MetadataOperation",
+		// fallback to String (as documented).
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::Glue::Table.IcebergInput",
 			PropertyName: "MetadataOperation",
@@ -164,7 +172,8 @@ func NewDefaultPatchManager() *PatchManager {
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::Glue::SecurityConfiguration.S3Encryptions",
 		}).
-		// References invalid, deleted type "AWS::Glue::SecurityConfiguration.S3Encryptions", fallback to JSON.
+		// References invalid, deleted type "AWS::Glue::SecurityConfiguration.S3Encryptions",
+		// fallback to JSON.
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::Glue::SecurityConfiguration.EncryptionConfiguration",
 			PropertyName: "S3Encryptions",
@@ -185,7 +194,8 @@ func NewDefaultPatchManager() *PatchManager {
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::LakeFormation::DataLakeSettings.Admins",
 		}).
-		// References invalid, deleted type "AWS::LakeFormation::DataLakeSettings.Admins", fallback to List(DataLakePrincipal) (as documented).
+		// References invalid, deleted type "AWS::LakeFormation::DataLakeSettings.Admins",
+		//fallback to List(DataLakePrincipal) (as documented).
 		RegisterRawPatch(&RawPatchFixPropertyType{
 			TypeName:     "AWS::LakeFormation::DataLakeSettings",
 			PropertyName: "Admins",
@@ -202,11 +212,49 @@ func NewDefaultPatchManager() *PatchManager {
 				ItemType:          "DataLakePrincipal",
 			},
 		}).
+		// Invalid type (referenced).
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::LakeFormation::DataLakeSettings.CreateDatabaseDefaultPermissions",
 		}).
+		// References invalid, deleted type "AWS::LakeFormation::DataLakeSettings.CreateDatabaseDefaultPermissions",
+		// fallback to List(PrincipalPermissions) (as documented).
+		RegisterRawPatch(&RawPatchFixPropertyType{
+			TypeName:     "AWS::LakeFormation::DataLakeSettings",
+			PropertyName: "CreateDatabaseDefaultPermissions",
+			ExpectedFields: &PropertyOrAttributeTypeFields{
+				PrimitiveType:     "",
+				Type:              "CreateDatabaseDefaultPermissions",
+				PrimitiveItemType: "",
+				ItemType:          "",
+			},
+			FixedFields: &PropertyOrAttributeTypeFields{
+				PrimitiveType:     "",
+				Type:              "List",
+				PrimitiveItemType: "",
+				ItemType:          "PrincipalPermissions",
+			},
+		}).
+		// Invalid type (referenced).
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::LakeFormation::DataLakeSettings.CreateTableDefaultPermissions",
+		}).
+		// References invalid, deleted type "AWS::LakeFormation::DataLakeSettings.CreateTableDefaultPermissions",
+		// fallback to List(PrincipalPermissions) (as documented).
+		RegisterRawPatch(&RawPatchFixPropertyType{
+			TypeName:     "AWS::LakeFormation::DataLakeSettings",
+			PropertyName: "CreateTableDefaultPermissions",
+			ExpectedFields: &PropertyOrAttributeTypeFields{
+				PrimitiveType:     "",
+				Type:              "CreateTableDefaultPermissions",
+				PrimitiveItemType: "",
+				ItemType:          "",
+			},
+			FixedFields: &PropertyOrAttributeTypeFields{
+				PrimitiveType:     "",
+				Type:              "List",
+				PrimitiveItemType: "",
+				ItemType:          "PrincipalPermissions",
+			},
 		}).
 		RegisterRawPatch(&RawPatchDeleteType{
 			TypeName: "AWS::LakeFormation::DataLakeSettings.ExternalDataFilteringAllowList",
